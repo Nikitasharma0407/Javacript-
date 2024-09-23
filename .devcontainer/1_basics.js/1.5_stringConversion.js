d8878a5f73a236f3377fe50bbc881bcb7f09c81a
Nikitasharma0407 <nikitasharma040702@gmail.com>
2024-09-23 13:28:37 +0000

    Conversion string

---
+++ .devcontainer/1_basics.js/1.5_stringConversion.js
@@ -0,0 +1,17 @@
+let str1="hello"
+let str2="Nikki"
+let str3= str1+str2
+console.log(str3);
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(+true)
+console.log(+"")
+let num1, num2,num3
+num1=num2=num3=3+2
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)
+console.log(num1)
+
