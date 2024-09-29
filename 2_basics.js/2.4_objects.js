7873325f5dc2cf100c4a5662a71e1ba020c20cdb
Nikitasharma0407 <nikitasharma040702@gmail.com>
2024-09-29 07:40:22 +0000

    objects

---
+++ 2_basics.js/2.4_objects.js
@@ -0,0 +1,27 @@
+const course={
+    coursename:" js in hindi",
+    price: "999",
+    courseInstructor:"hitesh"
+
+}
+//course.courseInstructure
+const {courseInstructor:instructor}=course
+console.log(instructor);
+// const navbar=({company})=>{  //de structuring
+
+// }
+// navbar(comapny="hitesh")
+
+// API= (put our work to another)
+// how to write values
+// {
+//     //JSON->OBJECT
+//     "name": "Hitesh",
+//     "course": " js in hindi",
+//     "price" : "free"    // both keys and values are strings->API call
+// }
+[
+    {},
+    {},
+    {}
+]   // in the form of array
