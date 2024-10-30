1dad5e42d0e0790ecb8b51a0fb6165b5c940679b
Nikitasharma0407 <nikitasharma040702@gmail.com>
2024-10-30 14:47:08 +0000

    control flow

---
+++ 2_basics.js/3.1_control_flow.js
@@ -0,0 +1,35 @@
+// const temp=42
+// if(temp<50){
+//     console.log("less than 50");
+
+// }else{
+//      console.log("greater than 50")
+// }
+// const score=200
+// if(score>100){
+//  let power="fly"
+//     console.log(`user power:${power}`);
+// }
+// console.log(`user power:${power}`);
+
+// const balance =1000
+// if(balance>500)
+//     console.log("test")  
+// switch case:
+const month=3
+switch(month){
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break; 
+    case 3:
+        console.log("march");
+        break;  
+    case 4:
+        console.log("april");
+        break;         
+    default:
+        break;    
+}
